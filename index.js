e9aa140d3122bf41d315ab1cade1120e07e98df3
AywinK <115905373+AywinK@users.noreply.github.com>
2023-01-29 09:00:53 +0000

    wip saves backup

--- index.js
+++ index.js
@@ -4,6 +4,7 @@ const Intern = require("./lib/Intern");
 const inquirer = require("inquirer");
 const path = require("path");
 const fs = require("fs");
+const PromptBuilder = require("./lib/PromptBuilder");
 
 const OUTPUT_DIR = path.resolve(__dirname, "output");
 const outputPath = path.join(OUTPUT_DIR, "team.html");
@@ -37,4 +38,51 @@ const render = require("./src/page-template.js");
 // initialise teamdata arr
 // prompt manager
 // THEN create manager obj -> push to array + START options prompt
-// THEN BRANCH into either |||create eng/int -> push to arr -> RETURN to options prompt||| OR |||html generation -> file write -> exit|||
+// THEN BRANCH into either |||create eng/int -> push to arr -> RETURN to options prompt (recursive? https://github.com/SBoudrias/Inquirer.js/blob/master/packages/inquirer/examples/recursive.js)||| OR |||html generation -> file write -> exit|||
+
+function init() {
+    console.log("Answer the following questions to generate your Team Profile file");
+
+    const teamDataArr = [];
+
+    const managerPromptArr =
+        new PromptBuilder("team manager")
+            .createNamePrompt()
+            .createIdPrompt().
+            createEmailPrompt()
+            .createOfficeNumberPrompt()
+            .createPromptsArr();
+
+
+
+    inquirer.prompt(managerPromptArr).then(data => {
+        console.log(data);
+        const { name, id, email, officeNumber } = data;
+        const managerObj = new Manager(name, id, email, officeNumber);
+        teamDataArr.push(managerObj);
+        console.log(teamDataArr);
+        inquirer.prompt(optionsPromptArr).then(data => { console.log(data) });
+    });
+
+    function generatesMenuPrompt() {
+
+        const optionsPromptArr =
+        new PromptBuilder()
+            .createOptionsPrompt()
+            .createPromptsArr();
+
+        inquirer.prompt(optionsPromptArr)
+        .then( data => {
+            switch (data.options) {
+                case "Engineer":
+                    generatesEngineerPrompt();
+                    case "Intern":
+                        generatesInternPrompt();
+                        case "Finish building the team - Generate Team Profile":
+                            startsExitProcess();
+            }
+        })
+    }
+}
+
+init();
